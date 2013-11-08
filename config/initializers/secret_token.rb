22dd0aa1ce07ec3d0a305ef9e284c14735a7598e
kvanderd <me@katharine.org>
2013-11-08 12:41:10 -0800

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Mia::Application.config.secret_token = '07e6ea2c299a9e8367967e5aa74bf53fecb4e552f29ee09f89be3f047e51d493fb9182fc786c61bd2c2c0d53fbd355d28dbaefa5396af131d325891f2279e620'
